11f7165d33447bea7edc9a253559a58d9c563fe8
Amit Gupta <amitgupta.gwl@gmail.com>
2020-06-21 15:03:52 +0530

    Add BG section scenario for clubBgSteps: true

--- test/Background_spec.js
+++ test/Background_spec.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 describe("Background", function () {
 
-    it("should throw error when scenario with no steps", async function(done) {
+    it("should trigger background event by default", async function(done) {
         const parser = new FeatureFileParser();
         const stepWords = [];
 
@@ -20,7 +20,38 @@ describe("Background", function () {
             stepWords.push(step.keyword);
         })
 
-        parser.on("end", () => {
+        parser.on("end", (result) => {
+            expect(result.feature.background.steps[0].statement).toBe("a string tokenizer");
+            expect(stepWords).toEqual(["Given", "When", "Then", "When", "Then"]);
+            done();
+        });
+
+        const filePath = path.join( __dirname, "./features/Background.feature");
+        const inputStream = fs.createReadStream( filePath );
+        parser.parseFile(inputStream) ;
+        //console.log(JSON.stringify(parser.output,null,4));
+    });
+
+    it("should not trigger background event then clubBgSteps: true", async function(done) {
+        const parser = new FeatureFileParser({
+            clubBgSteps: true
+        });
+        const stepWords = [];
+
+        parser.on("background", bg => {
+            done.fail();
+        })
+        
+        parser.on("scenario", scenario => {
+            expect(scenario.keyword).toBe("scenario");
+        })
+        
+        parser.on("step", step => {
+            stepWords.push(step.keyword);
+        })
+
+        parser.on("end", (result) => {
+            expect(result.feature.background).toBeNull();
             expect(stepWords).toEqual(["Given", "When", "Then", "When", "Then"]);
             done();
         });
